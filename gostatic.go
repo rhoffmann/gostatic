81fc11fa6b37a3bc0973125cd231f3dc406ea291
Richard Hoffmann <rjh.hoffmann@gmail.com>
2014-07-08 13:17:11 +0200

    first commit for heroku go app

---
+++ gostatic.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+	fs := http.FileServer(http.Dir("public"))
+	http.Handle("/", fs)
+	http.HandleFunc("/greet", greeter)
+	http.HandleFunc("/time", timeHandler)
+
+	log.Println("Listening...")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
+
+func root(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("public/index.html")
+	t.Execute(w, nil)
+}
+
+func greeter(w http.ResponseWriter, r *http.Request) {
+	username := r.FormValue("username")
+	t, _ := template.ParseFiles("public/greet.html")
+	err := t.Execute(w, username)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func timeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello. The time is: "+time.Now().Format(time.RFC850))
+	fmt.Fprintln(w, r.UserAgent())
+	fmt.Fprintln(w, r.URL.Path)
+}
